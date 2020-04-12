e356a18ad8b958b6f24dca8e3adda9430f803c8c
BecRoM <pillaete@gmail.com>
2020-04-12 12:39:12 +0200

    Arreglo en mensaje de bienvenida al cliente.

--- catalog/includes/languages/espanol/modules/content/create_account_success/cm_cas_message.php
+++ catalog/includes/languages/espanol/modules/content/create_account_success/cm_cas_message.php
@@ -14,7 +14,7 @@
   const MODULE_CONTENT_CAS_MESSAGE_DESCRIPTION  = 'Muestra un mensaje al cliente agradeciéndole por crear su perfil.';
   
   const MODULE_CONTENT_CAS_MESSAGE_PUBLIC_TITLE = <<<'EOT'
-<p class="lead">¡Enhorabuena! ¡Su cuenta ha sido creado con éxito! </p>
+<p class="lead">¡Enhorabuena! ¡Su cuenta ha sido creada con éxito!</p>
 <p>Ahora puede disfrutar de las ventajas de disponer de una cuenta para mejorar su experiencia de compra en línea con nosotros. Si tiene alguna pregunta sobre el funcionamiento de esta tienda, <a class="alert-link" href="%1$s">contáctenos</a> o vea su <a class="alert-link" href="%2$s">cuenta</a>.</p>
 <p>Se ha enviado una confirmación a su dirección de correo electrónico.</p>
 EOT;
