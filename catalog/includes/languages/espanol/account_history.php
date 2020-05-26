cab47b23d7306a9fa7de4cffa3d755558be1771d
BecRoM <pillaete@gmail.com>
2020-05-26 17:00:48 +0200

    Arreglos menores

--- catalog/includes/languages/espanol/account_history.php
+++ catalog/includes/languages/espanol/account_history.php
@@ -15,13 +15,13 @@ define('NAVBAR_TITLE_2', 'Pedidos');
 
 define('HEADING_TITLE', 'Todos Mis Pedidos');
 
-define('TEXT_ORDER_NUMBER', 'Número:');
-define('TEXT_ORDER_STATUS', 'Estado:');
-define('TEXT_ORDER_DATE', 'Fecha:');
-define('TEXT_ORDER_SHIPPED_TO', 'Enviado a:');
-define('TEXT_ORDER_BILLED_TO', 'Facturado a:');
-define('TEXT_ORDER_PRODUCTS', 'Productos:');
-define('TEXT_ORDER_COST', 'Precio:');
+define('TEXT_ORDER_NUMBER', 'Número');
+define('TEXT_ORDER_STATUS', 'Estado');
+define('TEXT_ORDER_DATE', 'Fecha');
+define('TEXT_ORDER_SHIPPED_TO', 'Enviado a');
+define('TEXT_ORDER_BILLED_TO', 'Facturado a');
+define('TEXT_ORDER_PRODUCTS', 'Productos');
+define('TEXT_ORDER_COST', 'Importe');
 define('TEXT_VIEW_ORDER', 'Ver Pedido');
 
 define('BUTTON_VIEW_ORDER', '<i title="Ver este Pedido" class="fas fa-eye"></i>');
