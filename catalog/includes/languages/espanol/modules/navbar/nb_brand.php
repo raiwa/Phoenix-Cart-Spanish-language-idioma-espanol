b1e74c93ce6d27cbcb426ff58cd82498f8cc0728
Rainer Schmied <info@oscaddons.com>
2021-01-27 11:57:03 +0100

    Update nb_brand.php

--- catalog/includes/languages/espanol/modules/navbar/nb_brand.php
+++ catalog/includes/languages/espanol/modules/navbar/nb_brand.php
@@ -14,7 +14,7 @@
   const MODULE_NAVBAR_BRAND_DESCRIPTION = <<<'EOT'
  Mostrar la marca de la tienda en el menú de navegación.  <div class="alert alert-warning">Esto puede ser un simple enlace o algo más complicado como una imagen.<br>
 <br>
-Para más detalles sobre el uso de imágenes, consulte <a target="_blank" rel="noreferrer" href="https://getbootstrap.com/docs/4.1/components/navbar/#brand"><u>navbar/#brand</u></a></div>
+Para más detalles sobre el uso de imágenes, consulte <a target="_blank" rel="noreferrer" href="https://getbootstrap.com/docs/4.6/components/navbar/#brand"><u>navbar/#brand</u></a></div>
 EOT;
 
   define('MODULE_NAVBAR_BRAND_PUBLIC_TEXT', STORE_NAME);
