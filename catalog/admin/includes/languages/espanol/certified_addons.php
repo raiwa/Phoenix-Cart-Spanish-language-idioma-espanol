f79cb1ec5f19959485ce2bcb06b61b9427212c0b
Rainer Schmied <info@oscaddons.com>
2021-08-08 10:48:43 +0200

    1.0.8.5

--- catalog/admin/includes/languages/espanol/certified_addons.php
+++ catalog/admin/includes/languages/espanol/certified_addons.php
@@ -10,18 +10,11 @@
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Addons y servicios certificados');
+const HEADING_TITLE = 'Proveedores certificados';
 
-define('TABLE_CERTIFIED_ADDONS_TITLE', 'Addon/Servicio');
-define('TABLE_CERTIFIED_ADDONS_OWNER', 'De');
-define('TABLE_CERTIFIED_ADDONS_RATING', 'Valoración');
-define('TABLE_CERTIFIED_ADDONS_REMARKS', 'Observaciones');
-define('TABLE_CERTIFIED_ADDONS_DATE', 'Fecha');
+const TABLE_CERTIFIED_ADDONS_TITLE = 'URL';
+const TABLE_CERTIFIED_ADDONS_OWNER = 'Proveedor';
+const TABLE_CERTIFIED_ADDONS_REMARKS = 'Observaciones';
 
-define('TEXT_CERTIFIED_ADDONS', '<p class="m-0 lead">Todos los Addons enumerados aquí han sido <strong>certificados</strong> por un miembro del Phoenix Core Team.</p>%s');
-define('NUM_CERTIFIED_ADDONS', 'Actualmente hay <b>%s</b> Addons/Servicios certificados.');
-
-define('TEXT_CERTIFIED_SEARCH_LABEL', 'Buscar');
-define('TEXT_CERTIFIED_SEARCH_PLACEHOLDER', 'empiece a escribir...');
-
-define('NUM_FILTERED_ADDONS', '<span class="filtered-result"><br>Filtrado a <b>{X}</b> Addon(s).</span>');
+const TEXT_CERTIFIED_ADDONS ='<p class="m-0 lead">Todos los Proveedores enumerados aquí han sido <strong>certificados</strong> por un miembro del Phoenix Core Team.</p>%s';
+const NUM_CERTIFIED_ADDONS = 'Actualmente hay <b>%s</b> Proveedores certificados.';
