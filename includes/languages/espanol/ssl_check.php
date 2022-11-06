cb540c390e5eb67f4644ccf57fac1fadeb8bbc41
Rainer Schmied <info@oscaddons.com>
2022-11-06 18:07:33 +0100

    Update ssl_check.php

--- includes/languages/espanol/ssl_check.php
+++ includes/languages/espanol/ssl_check.php
@@ -18,7 +18,7 @@ Hemos detectado que su navegador ha generado un Identificativo de Sesión Seguro
 Por razones de seguridad, deberá entrar de nuevo en su cuenta para verificar su identidad.<br><br>
 Algunos navegadores como Konqueror 3.1 no tienen la capacidad de generar automáticamente un ID de sesión SSL seguro, lo cual requerimos. Si utiliza un navegador de este tipo, le recomendamos que cambie a otro navegador, como <a class = "btn btn-light btn-sm" role="button" href="http://www.microsoft.com/ie/" target="_blank" rel="noreferrer">Microsoft Internet Explorer</a> o <a class = "btn btn-light btn-sm" role="button" href="http://channels.netscape.com/ns/browsers/download_other.jsp" target="_blank" rel="noreferrer">Netscape</a> o <a class = "btn btn-light btn-sm" role = "button" href = "http://www.mozilla.org/releases/" target="_blank" rel="noreferrer">Mozilla</a> para continuar con su experiencia de compra en línea.<br><br>
 Hemos tomado esta medida de seguridad para su beneficio y le pedimos disculpas por adelantado si se producen inconvenientes.<br><br>
-<a class="btn btn-success btn-sm" role="button" href="' . tep_href_link('contact_us.php') . '">póngase en contacto el propietario de la tienda</a> si tiene alguna pregunta relacionada con este requisito o si desea continuar comprando productos sin conexión.
+<a class="btn btn-success btn-sm" role="button" href="%s">póngase en contacto el propietario de la tienda</a> si tiene alguna pregunta relacionada con este requisito o si desea continuar comprando productos sin conexión.
 EOT;
 
 const BOX_INFORMATION_HEADING = 'Seguridad y Privacidad';
