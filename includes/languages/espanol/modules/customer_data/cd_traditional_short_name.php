ef49b72644f33594ec319e9fbfebf7397d0d92b3
Rainer Schmied <info@oscaddons.com>
2023-06-18 12:25:36 +0200

    Update cd_traditional_short_name.php

--- includes/languages/espanol/modules/customer_data/cd_traditional_short_name.php
+++ includes/languages/espanol/modules/customer_data/cd_traditional_short_name.php
@@ -12,3 +12,7 @@
 
 const MODULE_CUSTOMER_DATA_TRADITIONAL_SHORT_NAME_TEXT_TITLE = 'Nombre corto tradicional';
 const MODULE_CUSTOMER_DATA_TRADITIONAL_SHORT_NAME_TEXT_DESCRIPTION = 'Use el título y el apellido si está disponible o si no, el nombre.';
+
+const MODULE_CUSTOMER_DATA_TRADITIONAL_SHORT_NAME_GREET_MR = 'Estimado Sr. %s,' . "\n\n";
+const MODULE_CUSTOMER_DATA_TRADITIONAL_SHORT_NAME_GREET_MS = 'Estimada Señora Sra. %s,' . "\n\n";
+const MODULE_CUSTOMER_DATA_TRADITIONAL_SHORT_NAME_GREET_NONE = 'Estimad@ %s' . "\n\n";
